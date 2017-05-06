11168c28535d5104a6bcfb1c650af82a78f10a0a
Patricio Bruna <pbruna@itlinux.cl>
2017-05-06 12:27:21 -0300

    Fix Vagrant for tests

--- bin/getDownloadUrls.pl
+++ bin/getDownloadUrls.pl
@@ -1,8 +1,6 @@
 #!/bin/perl
 use Data::Dumper;
 my %urls = {};
-$urls{NETWORK} = {};
-$urls{OSS} = {};
 
 my $REPO_LIST_URL="https://raw.githubusercontent.com/jorgedlcruz/zimbra_bits/master/README.md";
 my $TEMP_DWL="/tmp/zimbra_urls";
@@ -14,6 +12,7 @@ my $DOWNLOAD_COMMAND="curl -k $REPO_LIST_URL | $URL_RGXP | awk -F \"$ZM_REPO_URL
 system($DOWNLOAD_COMMAND);
 open(URLS, $TEMP_DWL) or die "Couldn't open file $TEMP_DWL, $!";
 
+print "zimbra_installers:\n";
 while(<URLS>) {
 	chomp($_);
 	my $url = $_;
@@ -24,35 +23,9 @@ while(<URLS>) {
 	my $os_info = $file_info->{os_info};
 	my $file_extension = $file_info->{file_extension};
 	
-	if (!$urls{$zimbra_distro}{$version}) {
-		$urls{$zimbra_distro}{$version} = {};
-	}
-
-	if (!$urls{$zimbra_distro}{$version}{$os_info}) {
-		$urls{$zimbra_distro}{$version}{$os_info} = {};
-	}
-
-	if($version) {
-		$urls{$zimbra_distro}{$version}{$os_info} = $file;
-	}
-}
-
-print "_zimbra_urls:\n";
-printYAML("NETWORK");
-print "\n";
-printYAML("OSS");
-
-sub printYAML {
-	my ($zimbra_distro) = @_;
-	print("  \L$zimbra_distro:\n");
-	foreach my $zimbra_ver ( keys $urls{$zimbra_distro} ) {
-	  if ($zimbra_ver) {
-	  	print "    $zimbra_ver:\n";
-		
-		foreach my $os_info ( keys $urls{$zimbra_distro}{$zimbra_ver} ) {
-		  print "      $os_info: $urls{$zimbra_distro}{$zimbra_ver}{$os_info}\n";
-		}
-	  }
+	my $key_name = "ZCS_".$version."_".$zimbra_distro."_".$os_info;
+	if ($file) {
+		print "  $key_name: $file\n";
 	}
 }
 
